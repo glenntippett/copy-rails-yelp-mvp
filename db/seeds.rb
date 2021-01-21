e7a364798df249054ade9332e5e303ed7b98b54d
Glenn Tippett <glenn.tippett@gmail.com>
2021-01-21 15:21:36 +1100

    do some design

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+CATEGORY = %w[chinese italian japanese french belgian]
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+100.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: CATEGORY.sample,
+    phone_number: Faker::PhoneNumber.cell_phone
+    )
+  puts "Created #{restaurant.name}"
+end
+
+puts "All done!"
